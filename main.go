eb43e512de1fa344d9ca46a9b81804a044ffd367
Knevari <winresource000@gmail.com>
2024-02-24 11:53:59 -0300

    build: /games GET endpoint

--- main.go
+++ main.go
@@ -13,14 +13,20 @@ type game struct {
 	Rating float32 `json:"rating"`
 }
 
-func index(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{ "message": "Hello from Go" })
+var games = []game {
+	{ Id: 1, Title: "Elden Ring", Genre: "Action RPG", Rating: 5  },
+	{ Id: 2, Title: "God of War: Ragnarok", Genre: "Adventure", Rating: 5  },
+	{ Id: 3, Title: "Little Nighmares", Genre: "Horror", Rating: 5  },
+}
+
+func getGames(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, games);
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", index)
+	router.GET("/games", getGames)
 
 	router.Run("localhost:8080")
 }
